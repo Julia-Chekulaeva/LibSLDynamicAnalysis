1544f16352d14e2c4e19df06a21bdfc1520eef83
Julia <julia4eck@gmail.com>
2023-01-13 21:36:10 +0300

    Fix search param - file size, fix class name, add fun deleting logs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import org.jetbrains.research.libsl.LibSL
+import java.io.File
 
 
 const val lslPath = "src/main/resources"
@@ -8,5 +9,10 @@ const val propertyFileName = "C:/Users/cat_p/bot-j.github"
 fun main(args: Array<String>) {
     val libSL = LibSL(lslPath).loadFromFileName(lslFileName)
     val libName = "com.github.sealedtx:java-youtube-downloader"
-    GithubAccess(propertyFileName).analyseDataFromRepos(libName)
+    GitHubAccess(propertyFileName).analyseDataFromRepos(libName)
+    clearLogs()
+}
+
+private fun clearLogs() {
+    File(logsDirPath).listFiles()?.forEach { it.delete() }
 }
